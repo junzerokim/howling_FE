3fc4beda1e7c21bf0437e82850f5744f24db7a66
seooh <ohsh1115@gmail.com>
2023-03-16 00:31:42 +0900

    Add: MyFeed layout

--- src/Feed/MyFeedContent.jsx
+++ src/Feed/MyFeedContent.jsx
@@ -4,8 +4,18 @@ import styled from 'styled-components';
 function MyFeedContent() {
   return (
     <MyFeed>
-      <h1>MY FEED PAGE</h1>
-      <p>blahblahblahblahblahblahblahblah</p>
+      <Title>내가 작성한 글</Title>
+      <FeedContainer>
+        <Feed>
+          <p>TEXT</p>
+        </Feed>
+        <Feed>
+          <p>TEXT</p>
+        </Feed>
+        <Feed>
+          <p>TEXT</p>
+        </Feed>
+      </FeedContainer>
     </MyFeed>
   );
 }
@@ -18,4 +28,29 @@ const MyFeed = styled.div`
   justify-content: left;
   align-content: center;
   align-items: center;
+  gap: 30px;
+`;
+
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+  color: #333;
+`;
+
+const FeedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  /* position: absolute; */
+  gap: 10px;
+`;
+
+const Feed = styled.div`
+  width: 400px;
+  height: 200px;
+  border-radius: 10px;
+  background-color: #ccd3e6;
+  display: block;
 `;
