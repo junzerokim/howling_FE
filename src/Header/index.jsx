62f154e1372fa7b28f5499f3ea69cd736c711560
seooh <ohsh1115@gmail.com>
2023-03-16 15:15:59 +0900

    Fix: Header layout

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -3,31 +3,47 @@ import styled from 'styled-components';
 import { GiWolfHowl } from 'react-icons/gi';
 import Profile from '../Modal/ProfileModal';
 
+function HiddenLogo() {
+  return <HiddenLogoContainer>a</HiddenLogoContainer>;
+}
 function Logo() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        fontSize: '30px',
-        fontStyle: 'italic',
-        color: 'rgb(64, 63, 63)',
-      }}
-    >
+    <LogoContainer>
       <GiWolfHowl size="50" style={{ color: 'rgb(22, 64, 123)' }} />
       Howling
-    </div>
+    </LogoContainer>
   );
 }
 
 function Header() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+    <HeaderContainer>
+      <HiddenLogo />
       <Logo />
       <Profile />
-    </div>
+    </HeaderContainer>
   );
 }
 
 export default Header;
+
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 30px;
+  font-style: italic;
+  color: rgb(64, 63, 63);
+`;
+
+const HiddenLogoContainer = styled.div`
+  color: white;
+  width: 70px;
+  height: 70px;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
